36456766d234dfd3d83ce09121859d931438cc24
Gogs <gogs@fake.local>
2020-06-20 06:57:09 +0000

    tried

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use pcap::Device;
 use std::collections::HashMap;
+use rawsock::open_best_library;
 use std::time::Instant;
 use std::env;
 
@@ -189,6 +190,10 @@ fn notify_discord(intruder: &DecodedPacket) {
 }
 
 fn main() {
+    dynamic_capture();
+}
+
+fn regular_capture() {
 
     // Parse Config
     let config = Config::parse().unwrap();
@@ -225,5 +230,26 @@ fn main() {
         // Increment the threshold counter for this offending IP
         counter.inc(&packet.src, &packet);
 
+    }
+}
+
+fn dynamic_capture() {
+    println!("Opening packet capturing library");
+    let lib = open_best_library().expect("Could not open any packet capturing library");
+    println!("Library opened, version is {}", lib.version());
+    let interf_name = lib.all_interfaces()
+        .expect("Could not obtain interface list").first()
+        .expect("There are no available interfaces").name.clone();
+    println!("Opening the {} interface", &interf_name);
+    let mut interf = lib.open_interface(&interf_name).expect("Could not open network interface");
+    println!("Interface opened, data link: {}", interf.data_link());
+
+    interf.set_filter("icmp").expect("Could not set filter");
+
+    //receive some packets.
+    println!("Receiving 5 icmp packets:");
+    for _ in 0..5 {
+        let packet = interf.receive().expect("Could not receive packet");
+        println!("Received icmp packet: {}", packet);
     }
 }
